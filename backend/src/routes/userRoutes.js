43d8581d51e1ec192d1dd9cd3d0520557760ac9f
handsome1123 <6531501227@lamduan.mfu.ac.th>
2025-10-26 21:36:29 +0700

    loginpage

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const { listUsers } = require('../controllers/userController');
-const { authenticate, authorizeRole } = require('../middlewares/authMiddleware');
-
-router.get('/', authenticate, authorizeRole(['admin']), listUsers);
-
-module.exports = router;
